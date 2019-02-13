eefbb2bc96e9ce272236e5a0a5adca1d454545e4
Ben Poling <benpoling@Bens-MacBook-Pro.local>
2019-02-13 15:54:40 -0800

    clear list button functionality

--- app/appToDo.js
+++ app/appToDo.js
@@ -19,38 +19,40 @@ $(document).ready(function(event) {
   	var $toDo = $('.input-toDo').val();
   	var $toDoID = $('.input-toDo').val()
   	$toDoID = $toDoID.split(' ').join('-');
+  	var liClass = 'toDoItem'
     var deleteClass = 'btn-delete'
-    var $listItem = `<li class=${$toDo}>${$toDo}<button class=${deleteClass} id=${$toDoID}>delete</button></li>`;
-    // $listItem.addClass($toDo);
+    var $listItem = `<li class=${liClass}>${$toDo}<button class=${deleteClass} id=${$toDoID}>delete</button></li>`;
   	if($toDo === '') {
-  		alert('No To Do Item!')
+  	  alert('No To Do Item!')
   	} else if(localStorage.getItem('dataToDo') === null){
       var objToDo = {}
       objToDo[$toDo] = $listItem;
       $($listItem).appendTo('.container-toDoList')
       localStorage.setItem('dataToDo', JSON.stringify(objToDo));
+      $('.input-toDo').val('');
   	} else if(!JSON.parse(localStorage.getItem('dataToDo')).hasOwnProperty($toDo)) {
   	  var objToDo = JSON.parse(localStorage.getItem('dataToDo'))
   	  objToDo[$toDo] = $listItem;
       objToDo = JSON.stringify(objToDo)
       localStorage.setItem('dataToDo', objToDo);
       $($listItem).appendTo('.container-toDoList')
+      $('.input-toDo').val('');
   	}
-  	// console.log($toDo)
   })
 
   $('.container-toDoList').on('click', '.btn-delete', function(event) {
   	var key = event.currentTarget.id;
   	key = key.split('-').join(' ');
   	var objToDo = JSON.parse(localStorage.getItem('dataToDo'))
-  	// console.log(key);
-  	// console.log(objToDo[key]);
   	delete objToDo[key];
   	objToDo = JSON.stringify(objToDo);
   	localStorage.setItem('dataToDo', objToDo);
   	reloadToDo();
   })
 
-})
+  $('.btn-clear').on('click', function(event) {
+    localStorage.removeItem('dataToDo');
+    reloadToDo();
+  })
 
-      // <li> thing <button>delete</button></li>
+})
