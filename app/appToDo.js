c6da29c854b45cda9734c5fa3c7e3fa1a47f0802
Ben Poling <benpoling@Bens-MacBook-Pro.local>
2019-02-13 15:35:42 -0800

    delete to do implemented/reload to dos

--- app/appToDo.js
+++ app/appToDo.js
@@ -1,14 +1,31 @@
 $(document).ready(function(event) {
   // var deleteClass = 'btn-delete'
   // var $listItem = `<li> <button class=${deleteClass}>delete</button></li>`
+  var reloadToDo = function() {
+  	$('.container-toDoList').empty();
+    var toDoObj = JSON.parse(localStorage.getItem('dataToDo'));
+    // if(toDoObj === undefined) {
+    // 	$('')
+    // }
+    for(var key in toDoObj) {
+      $(toDoObj[key]).appendTo('.container-toDoList')
+    }
+  }
+  if(localStorage.getItem('dataToDo') != null) {
+    reloadToDo();
+  }
+
   $('.btn-addToDo').on('click', function(event) {
   	var $toDo = $('.input-toDo').val();
-    var deleteClass = 'btn-delete';
-    var $listItem = `<li class=${$toDo}>${$toDo}<button class=${deleteClass}>delete</button></li>`;
+  	var $toDoID = $('.input-toDo').val()
+  	$toDoID = $toDoID.split(' ').join('-');
+    var deleteClass = 'btn-delete'
+    var $listItem = `<li class=${$toDo}>${$toDo}<button class=${deleteClass} id=${$toDoID}>delete</button></li>`;
+    // $listItem.addClass($toDo);
   	if($toDo === '') {
   		alert('No To Do Item!')
   	} else if(localStorage.getItem('dataToDo') === null){
-  	  var objToDo = {}
+      var objToDo = {}
       objToDo[$toDo] = $listItem;
       $($listItem).appendTo('.container-toDoList')
       localStorage.setItem('dataToDo', JSON.stringify(objToDo));
@@ -23,7 +40,15 @@ $(document).ready(function(event) {
   })
 
   $('.container-toDoList').on('click', '.btn-delete', function(event) {
-  	console.log('hello')
+  	var key = event.currentTarget.id;
+  	key = key.split('-').join(' ');
+  	var objToDo = JSON.parse(localStorage.getItem('dataToDo'))
+  	// console.log(key);
+  	// console.log(objToDo[key]);
+  	delete objToDo[key];
+  	objToDo = JSON.stringify(objToDo);
+  	localStorage.setItem('dataToDo', objToDo);
+  	reloadToDo();
   })
 
 })
